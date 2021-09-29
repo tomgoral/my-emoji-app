032fe6692b0ed65c374ca8eb200faba7656cbe08
Tom Goral <tgoral@acument.com>
2021-09-29 13:09:01 -0400

    Added emojis
    added extra goodness

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const myEmojis = ["👨‍💻", "⛷", "🍲","🏗","👌","🎃"]
 
 function renderEmojis() {
     const emojiContainer = document.getElementById("emoji-container")
